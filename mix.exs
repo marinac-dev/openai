bb1c6b2a4e9a73108c94bf4b82266f7942ffd83f
marinac-dev <github@regex-sh.com>
2023-03-26 03:45:44 +0200

    chore: Bump version
    Signed-off-by: marinac-dev <github@regex-sh.com>

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule OpenAi.MixProject do
   def project do
     [
       app: :openai,
-      version: "0.1.0",
+      version: "0.0.1",
       elixir: "~> 1.14",
       start_permanent: Mix.env() == :prod,
       deps: deps()
