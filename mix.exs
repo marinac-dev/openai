6aeb144abb5ac06addeff9ff772b5a90d8809329
marinac-dev <github@regex-sh.com>
2023-03-26 03:44:15 +0200

    chore: Add dependencies
    Signed-off-by: marinac-dev <github@regex-sh.com>

--- mix.exs
+++ mix.exs
@@ -14,6 +14,7 @@ defmodule OpenAi.MixProject do
   # Run "mix help compile.app" to learn about applications.
   def application do
     [
+      mod: {OpenAi, []},
       extra_applications: [:logger]
     ]
   end
@@ -21,8 +22,13 @@ defmodule OpenAi.MixProject do
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
-      # {:dep_from_hexpm, "~> 0.3.0"},
-      # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
+      {:ex_doc, "~> 0.29.2", only: :dev},
+      {:finch, "~> 0.15.0"},
+      {:idna, "~> 6.0"},
+      {:castore, "~> 0.1"},
+      # * Code quality
+      {:credo, "~> 1.6", only: [:dev, :test], runtime: false},
+      {:dialyxir, "~> 1.0", only: [:dev], runtime: false}
     ]
   end
 end
