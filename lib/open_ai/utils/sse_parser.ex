57a0780a9cf30ef4c5c0fa1f6a9a6737f64873b2
marinac-dev <github@regex-sh.com>
2023-04-03 19:43:41 +0200

    feat(utils): Add Server Sent events parser for OpenAi streams
    Signed-off-by: marinac-dev <github@regex-sh.com>

---
+++ lib/open_ai/utils/sse_parser.ex
@@ -0,0 +1,33 @@
+defmodule OpenAi.Utils.SseParser do
+  @moduledoc """
+  Parses a server-sent event stream into a string.
+  """
+
+  @spec parse(list()) :: String.t()
+  def parse(response) do
+    response
+    |> Enum.reverse()
+    |> Enum.join()
+    |> String.split("data: ")
+    |> Enum.reduce([], fn
+      "", acc -> acc
+      "[DONE]" <> _, acc -> acc
+      data, acc -> [parse_data(data) | acc]
+    end)
+    |> Enum.reverse()
+    |> Enum.join()
+  end
+
+  defp parse_data(data) do
+    data
+    |> Jason.decode!()
+    |> Map.get("choices")
+    |> List.first()
+    |> Map.get("delta")
+    |> parse_delta()
+  end
+
+  defp parse_delta(%{"role" => "assistant"}), do: ""
+  defp parse_delta(%{"content" => content}), do: content
+  defp parse_delta(%{}), do: ""
+end
