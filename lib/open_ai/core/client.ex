6bc0d92f1d8efaed27389597484108928d47bcd2
marinac-dev <github@regex-sh.com>
2023-04-21 03:21:28 +0200

    feat(openai): Add headers to client and add delete request
    Signed-off-by: marinac-dev <github@regex-sh.com>

--- lib/open_ai/core/client.ex
+++ lib/open_ai/core/client.ex
@@ -2,14 +2,16 @@ defmodule OpenAi.Core.Client do
   defmacro __using__(_opts) do
     quote do
       @doc false
-      def request_builder(method, path, body, params) do
+      def request_builder(method, path, body, headers, params) do
         full_url = api_url() <> api_path() <> path
 
-        headers = [
-          {"OpenAI-Organization", get_organization()},
-          {"Authorization", "Bearer #{api_key()}"},
-          {"Content-Type", "application/json"}
-        ]
+        base = %{
+          "OpenAI-Organization" => get_organization(),
+          "Authorization" => api_key(),
+          "Content-Type" => "application/json"
+        }
+
+        headers = Map.merge(base, headers) |> Map.to_list()
 
         Finch.build(method, full_url, headers, body, params)
       end
@@ -24,13 +26,13 @@ defmodule OpenAi.Core.Client do
       defp api_key do
         case Application.get_env(:openai, :api_key) do
           nil -> raise "OpenAI API key is not set"
-          api_key -> api_key
+          api_key -> "Bearer #{api_key}"
         end
       end
 
       defp api_url, do: "https://api.openai.com"
 
-      import(OpenAi.Core.Client, only: [scope: 1, get: 3, post: 3, stream: 6])
+      import OpenAi.Core.Client
     end
   end
 
@@ -41,23 +43,30 @@ defmodule OpenAi.Core.Client do
     end
   end
 
-  defmacro get(path, body, params) do
+  defmacro get(path, body, headers, params) do
     quote do
-      request_builder(:get, unquote(path), unquote(body), unquote(params))
+      request_builder(:get, unquote(path), unquote(body), unquote(headers), unquote(params))
       |> Finch.request(OpenAiFinch)
     end
   end
 
-  defmacro post(path, body, params) do
+  defmacro post(path, body, headers, params) do
     quote do
-      request_builder(:post, unquote(path), unquote(body), unquote(params))
+      request_builder(:post, unquote(path), unquote(body), unquote(headers), unquote(params))
+      |> Finch.request(OpenAiFinch)
+    end
+  end
+
+  defmacro delete(path, body, headers, params) do
+    quote do
+      request_builder(:delete, unquote(path), unquote(body), unquote(headers), unquote(params))
       |> Finch.request(OpenAiFinch)
     end
   end
 
   defmacro stream(method, path, body, params, acc, callback) do
     quote do
-      request_builder(unquote(method), unquote(path), unquote(body), unquote(params))
+      request_builder(unquote(method), unquote(path), unquote(body), %{}, unquote(params))
       |> Finch.stream(OpenAiFinch, unquote(acc), unquote(callback))
     end
   end
